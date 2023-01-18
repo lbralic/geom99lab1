57280f61d2ac392f8dab4db7f01c326bfd7eaaec
lbralic <114246886+lbralic@users.noreply.github.com>
2023-01-17 21:16:37 -0500

    Update clustering.js

--- clustering/clustering.js
+++ clustering/clustering.js
@@ -1,7 +1,7 @@
 function initMap() {
   const map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 3,
-    center: { lat: -28.024, lng: 140.887 },
+    zoom: 5,
+    center: { lat: -49.885, lng: -84.395 },
   });
   // Create an array of alphabetical characters used to label the markers.
   const labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
