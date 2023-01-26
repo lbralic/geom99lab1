24dbdb7cb66f68921c77ac6afe4c5192662b1187
lbralic <114246886+lbralic@users.noreply.github.com>
2023-01-26 16:56:28 -0500

    Update myscript.js

--- story/myscript.js
+++ story/myscript.js
@@ -8,51 +8,54 @@ function initMap() {
     });
     
     
+const contentRome =
+  '<div id="content">' +
+  '<div id="siteNotice">' +
+  "</div>" +
+  '<h1 id="firstHeading" class="firstHeading">Rome</h1>' +
+  '<div id="bodyContent">' +
+  "<p><b>Rome</b>, also referred to as <b>Ayers Rock</b>, is a large " +
+  "sandstone rock formation in the southern part of the " +
+  "Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) " +
+  "south west of the nearest large town, Alice Springs; 450&#160;km " +
+  "(280&#160;mi) by road. Kata Tjuta and Uluru are the two major " +
+  "features of the Uluru - Kata Tjuta National Park. Uluru is " +
+  "sacred to the Pitjantjatjara and Yankunytjatjara, the " +
+  "Aboriginal people of the area. It has many springs, waterholes, " +
+  "rock caves and ancient paintings. Uluru is listed as a World " +
+  "Heritage Site.</p>" +
+  '<p>Attribution: Uluru, <a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">' +
+  "https://en.wikipedia.org/w/index.php?title=Uluru</a> " +
+  "(last visited June 22, 2009).</p>" +
+  "</div>" +
+  "</div>";
+    
     // Inspiration: https://developers.google.com/maps/documentation/javascript/examples/infowindow-simple
     // advanced-markers-accessibility from https://googlemaps.github.io/js-samples/
     
+    // Markers
     const numMarkers = [
     {
       position: rome,
       title: "Rome",
+      content: contentRome,
     },
     {
       position: san_gimi,
       title: "San Gimignano",
+      content: contentRome,
     },
     {
       position: florence,
       title: "Florence",
+      content: contentRome,
     }
   ];
     
-    const contentRome =
-      '<div id="content">' +
-      '<div id="siteNotice">' +
-      "</div>" +
-      '<h1 id="firstHeading" class="firstHeading">Rome</h1>' +
-      '<div id="bodyContent">' +
-      "<p><b>Rome</b>, also referred to as <b>Ayers Rock</b>, is a large " +
-      "sandstone rock formation in the southern part of the " +
-      "Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) " +
-      "south west of the nearest large town, Alice Springs; 450&#160;km " +
-      "(280&#160;mi) by road. Kata Tjuta and Uluru are the two major " +
-      "features of the Uluru - Kata Tjuta National Park. Uluru is " +
-      "sacred to the Pitjantjatjara and Yankunytjatjara, the " +
-      "Aboriginal people of the area. It has many springs, waterholes, " +
-      "rock caves and ancient paintings. Uluru is listed as a World " +
-      "Heritage Site.</p>" +
-      '<p>Attribution: Uluru, <a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">' +
-      "https://en.wikipedia.org/w/index.php?title=Uluru</a> " +
-      "(last visited June 22, 2009).</p>" +
-      "</div>" +
-      "</div>";
-    const infowindow = new google.maps.InfoWindow({
-      content: contentRome,
-      ariaLabel: "Rome",
-    });
+    // Info window to share between markers
+    const infoWindow = new google.maps.InfoWindow();
     
-  
+    // Create the markers
     numMarkers.forEach(({ position, title }, i) => {
     const pinView = new google.maps.marker.PinView({
       glyph: `${i + 1}`,
@@ -62,7 +65,7 @@ function initMap() {
     const marker = new google.maps.AdvancedMarkerView({
       position,
       map,
-      title: `${i + 1}. ${title}`,
+      title: `${i + 1}. ${title}.`,
       content: pinView.element,
     });
   
