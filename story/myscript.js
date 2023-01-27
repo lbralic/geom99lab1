cfd76d84617994b8afd9ba2b0b4fff753f78f24e
lbralic <114246886+lbralic@users.noreply.github.com>
2023-01-26 19:36:04 -0500

    Update myscript.js

--- story/myscript.js
+++ story/myscript.js
@@ -45,19 +45,9 @@ function initMap() {
       "</div>" +
       '<h1 id="firstHeading" class="firstHeading">San Gimignano</h1>' +
       '<div id="bodyContent">' +
-      "<p><b>Rome</b>, also referred to as <b>Ayers Rock</b>, is a large " +
-      "sandstone rock formation in the southern part of the " +
-      "Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) " +
-      "south west of the nearest large town, Alice Springs; 450&#160;km " +
-      "(280&#160;mi) by road. Kata Tjuta and Uluru are the two major " +
-      "features of the Uluru - Kata Tjuta National Park. Uluru is " +
-      "sacred to the Pitjantjatjara and Yankunytjatjara, the " +
-      "Aboriginal people of the area. It has many springs, waterholes, " +
-      "rock caves and ancient paintings. Uluru is listed as a World " +
-      "Heritage Site.</p>" +
-      '<p>Attribution: Uluru, <a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">' +
-      "https://en.wikipedia.org/w/index.php?title=Uluru</a> " +
-      "(last visited June 22, 2009).</p>" +
+      "<p>San Gimignano is a small village located within Tuscany, and it is where " +
+      "I spent a large portion of my trip. I got a tour of the lush vinyards in this region, " +
+      "and I got the chance to learn about the wine-making process.</p>"
       "</div>" +
       "</div>";
     const infowindow2 = new google.maps.InfoWindow({
@@ -88,19 +78,10 @@ function initMap() {
       "</div>" +
       '<h1 id="firstHeading" class="firstHeading">Florence</h1>' +
       '<div id="bodyContent">' +
-      "<p><b>Rome</b>, also referred to as <b>Ayers Rock</b>, is a large " +
-      "sandstone rock formation in the southern part of the " +
-      "Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) " +
-      "south west of the nearest large town, Alice Springs; 450&#160;km " +
-      "(280&#160;mi) by road. Kata Tjuta and Uluru are the two major " +
-      "features of the Uluru - Kata Tjuta National Park. Uluru is " +
-      "sacred to the Pitjantjatjara and Yankunytjatjara, the " +
-      "Aboriginal people of the area. It has many springs, waterholes, " +
-      "rock caves and ancient paintings. Uluru is listed as a World " +
-      "Heritage Site.</p>" +
-      '<p>Attribution: Uluru, <a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">' +
-      "https://en.wikipedia.org/w/index.php?title=Uluru</a> " +
-      "(last visited June 22, 2009).</p>" +
+      "<p>My final stop was Florence, where I visited family. I got to see " +
+      "the Cathedral of Santa Maria del Fiore, The Boboli Gardens, The Pitti " +
+      "Palace, and the Uffizi Gallery. This was favourite stop, and I would " +
+      "like to visit again sometime in the future.</p>" +
       "</div>" +
       "</div>";
     const infowindow3 = new google.maps.InfoWindow({
